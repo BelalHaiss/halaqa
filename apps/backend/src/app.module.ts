cb607c9977f7650730a85ab062d93fb6bc95d253
belalhaiss <belalhaiss@gmail.com>
2026-02-15 11:54:24 +0200

    [feat] add roles guards [clean ]unused controller and service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { SessionModule } from './modules/session/session.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { ScheduleModule } from '@nestjs/schedule';
+import { RolesGuard } from './modules/auth/roles.guard';
 
 @Module({
   imports: [
@@ -19,14 +17,15 @@ import { ScheduleModule } from '@nestjs/schedule';
     DatabaseModule,
     AuthModule,
     UserModule,
-    SessionModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
